60b5ea0b8704b402dfb27c620259bb8c282fad45
local student <student@studentservers.JAC-SERVERS.JOHNABBOTT.QC.CA>
2017-05-01 19:04:58 -0400

    Initial commit

---
+++ app/src/main/java/com/example/student/floatinglabeledittext/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.student.floatinglabeledittext;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickMethod(View view) {
+//        Toast.makeText(getBaseContext(), "This is my Toast message!",
+//            Toast.LENGTH_LONG).show();
+
+    }
+
+
+
+public void onRadioButton2clicked(android.view.View view) {}}
